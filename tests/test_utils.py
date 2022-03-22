436a36f905e97051acb7103abcbdf4244070899c
Rocky Gu <x_forever@126.com>
2022-03-22 10:59:17 +0800

    update unittest cases for function hexdump
    Signed-off-by: Rocky Gu <x_forever@126.com>

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,13 +3,74 @@ from utils import hexdump, hexundump
 
 class TestHexdumpMethods(unittest.TestCase):
     def test_hexdump(self):
-        print("Test hexdump")
+        data = b'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        expect = '''
+hexundump("""
+0000   41 42 43 44 45 46 47 48 49 4A 4B 4C 4D 4E 4F 50   ABCDEFGHIJKLMNOP
+0010   51 52 53 54 55 56 57 58 59 5A                     QRSTUVWXYZ
+""", linesize=16)
+        '''
+        self.assertEqual(expect.strip(), hexdump(data).strip())
+
+        data = b'0' * 100
+        expect = '''
+hexundump("""
+0000   30 30 30 30 30 30 30 30 30 30 30 30 30 30 30 30   0000000000000000
+0010   30 30 30 30 30 30 30 30 30 30 30 30 30 30 30 30   0000000000000000
+0020   30 30 30 30 30 30 30 30 30 30 30 30 30 30 30 30   0000000000000000
+0030   30 30 30 30 30 30 30 30 30 30 30 30 30 30 30 30   0000000000000000
+0040   30 30 30 30 30 30 30 30 30 30 30 30 30 30 30 30   0000000000000000
+0050   30 30 30 30 30 30 30 30 30 30 30 30 30 30 30 30   0000000000000000
+0060   30 30 30 30                                       0000
+""", linesize=16)
+        '''
+        self.assertEqual(expect.strip(), hexdump(data).strip())
+
+        data = bytes([
+            0x03, 0xf3, 0x0d, 0x0a, 0x43, 0x7d, 0x38, 0x62, 0x63, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x03, 0x00, 0x00, 0x00, 0x40, 0x00, 0x00, 0x00, 0x73, 0x42, 0x00, 0x00, 0x00, 0x64, 0x00,
+            0x00, 0x64, 0x01, 0x00, 0x6c, 0x00, 0x00, 0x5a, 0x00, 0x00, 0x64, 0x02, 0x00, 0x65, 0x00, 0x00,
+            0x6a, 0x01, 0x00, 0x66, 0x01, 0x00, 0x64
+        ])
+        expect = '''
+hexundump("""
+0000   03 F3 0D 0A 43 7D 38 62 63 00 00 00 00 00 00 00   ....C}8bc.......
+0010   00 03 00 00 00 40 00 00 00 73 42 00 00 00 64 00   .....@...sB...d.
+0020   00 64 01 00 6C 00 00 5A 00 00 64 02 00 65 00 00   .d..l..Z..d..e..
+0030   6A 01 00 66 01 00 64                              j..f..d
+""", linesize=16)
+        '''
+        self.assertEqual(expect.strip(), hexdump(data).strip())
+
+        expect = '''
+hexundump("""
+0000   03 F3 0D 0A 43 7D 38 62 63 00   ....C}8bc.
+000A   00 00 00 00 00 00 00 03 00 00   ..........
+0014   00 40 00 00 00 73 42 00 00 00   .@...sB...
+001E   64 00 00 64 01 00 6C 00 00 5A   d..d..l..Z
+0028   00 00 64 02 00 65 00 00 6A 01   ..d..e..j.
+0032   00 66 01 00 64                  .f..d
+""", linesize=10)
+        '''
+        self.assertEqual(expect.strip(), hexdump(data, linesize=10).strip())
+
+        expect = '''
+hexundump("""
+0000   03 F3 0D 0A 43 7D 38 62   ....C}8b
+0008   63 00 00 00 00 00 00 00   c.......
+0010   00 03 00 00 00 40 00 00   .....@..
+0018   00 73 42 00 00 00 64 00   .sB...d.
+0020   00 64 01 00 6C 00 00 5A   .d..l..Z
+0028   00 00 64 02 00 65 00 00   ..d..e..
+0030   6A 01 00 66 01 00 64      j..f..d
+""", linesize=8)
+        '''
+        self.assertEqual(expect.strip(), hexdump(data, linesize=8).strip())
 
     def test_hexundump(self):
+        # TEST: data == hexundump(hexdump(data), linesize=16)
         for i in range(35):
             data = b'0' * i
-            print(hexdump(data))
-            # TEST: data == hexundump(hexdump(data), linesize=16)
             self.assertEqual(data, hexundump(hexdump(data), linesize=16))
 
         data = b'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
